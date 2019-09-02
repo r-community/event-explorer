d5043d6b77179625db55893b697bc978d50c21e5
Ben <ben@rpowerlabs.org>
2019-09-02 20:17:21 +0100

    Create satrdays.R

---
+++ R/satrdays.R
@@ -0,0 +1,6 @@
+satrd <- read.csv("satrdays.csv", header = TRUE)
+
+satrd_cal <-  satrd[c("Event", "Date", "URL", "Additional_Info", "Owner")]
+colnames(satrd_cal) <- c("title","start", "url", "additional_info", "owner")
+satrday_json <- jsonlite::toJSON(satrd_cal, pretty = TRUE )
+writeLines(satrday_json, "docs/data/satrdays.json")
